628b03f797ac8d764e39cf809692a9619edc040a
vhoangfbg <vhoangfbg@gmail.com>
2025-03-17 04:32:31 +0700

    fix little bug 2

--- obj/Debug/net8.0/spam_messenger.AssemblyInfo.cs
+++ obj/Debug/net8.0/spam_messenger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("spam_messenger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a433d5858162bb0a39fd0acd996f27fa4809d5d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+103a0163a2a59908cbf287541e9997601689dc31")]
 [assembly: System.Reflection.AssemblyProductAttribute("spam_messenger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("spam_messenger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
